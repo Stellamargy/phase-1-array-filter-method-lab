989a36c9a1177de0774d30d3ce2b2c2c73f8ec33
stlmargy <stellahmargy0211@gmail.com>
2024-07-03 15:11:59 +0300

    completes filter lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers,nameofDriver){
+   return drivers.filter((driver)=>driver.toLowerCase()===nameofDriver.toLowerCase())
+}
+
+// console.log(findMatching(["Stella" ,"Margy"], "stella"))
+
+function fuzzyMatch(drivers , characters){
+    const len=characters.length;
+    return drivers.filter((driver)=>driver.startsWith(characters))
+}
+
+// console.log(fuzzyMatch(["Stella", "Margy", "Oloo"], "Mar"))
+
+function matchName(drivers , name){
+    return drivers.filter((driver)=>driver.name===name)
+
+}
